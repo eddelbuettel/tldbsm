dfce883132489fb80209ab88a713828bf9769952
Dirk Eddelbuettel <edd@debian.org>
2023-05-05 08:20:13 -0500

    Adjust to local include paths

--- inst/include/tiledbsoma_types.h
+++ inst/include/tiledbsoma_types.h
@@ -4,6 +4,6 @@
 #include <nanoarrow.h>          // for C interface to Arrow
 #include <tiledb/tiledb>	// for QueryCondition etc
 #define ARROW_SCHEMA_AND_ARRAY_DEFINED 1
-#include <tiledbsoma/tiledbsoma>
+#include "tiledbsoma/tiledbsoma"
 
 namespace tdbs = tiledbsoma;
